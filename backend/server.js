a3d92de34cb796f48d65af160755e80017380552
veera2299 <168623246+veera2299@users.noreply.github.com>
2024-09-02 21:26:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from './routes/orderRoute.js';
 //app config
 const app = express();
 
-const port = 4000 ;
+const port = 4000 || process.env.PORT ;
 
 //middleware
 app.use(express.json());
