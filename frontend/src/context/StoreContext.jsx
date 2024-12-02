dae97a85cc92230eeea410e18080175b9280b703
veerababu <penkeyveerababu229@gmail.com>
2024-12-02 22:18:56 +0530

    update backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://online-food-backend-0uyi.onrender.com";
     const [token,setToken] = useState("");
     const [food_list,setFood_list] = useState([]);
 
